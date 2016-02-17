1f77178067e4fd97f43960718c5e619e86493ba9
Chris Hall <christopherhall17@gmail.com>
2016-02-17 21:01:23 +0000

    Generate Contact form from model using WTForm-Alchemy

--- app/core/controller.py
+++ app/core/controller.py
@@ -10,8 +10,9 @@ core = Blueprint('core', __name__, template_folder='templates/core')
 @core.route('/contact/create', methods=['GET', 'POST'])
 def create_contact():
     form = CreateContact(request.form)
-    if form.validate_on_submit():
-        contact = Contact(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data)
-        db.session.add(contact)
-        db.session.commit()
-    return render_template('core/contact.html', form=form)
+    if request.method == 'POST':
+        if form.validate():
+            contact = Contact(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data)
+            db.session.add(contact)
+            db.session.commit()
+    return render_template('core/contact.html', form=form, fields=form.data.keys())
--- app/core/forms.py
+++ app/core/forms.py
@@ -1,11 +1,8 @@
-from flask_wtf import Form
-from wtforms import StringField
-from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired
+from wtforms_alchemy import ModelForm
+
+from app.core.models import Contact
 
 
-class CreateContact(Form):
-
-    first_name = StringField('First Name', [DataRequired(message='First Name is required.')])
-    last_name = StringField('Last Name', [DataRequired(message='Last Name is required.')])
-    email = EmailField('Email', [DataRequired(message='Email is required.')])
+class CreateContact(ModelForm):
+    class Meta:
+        model = Contact
